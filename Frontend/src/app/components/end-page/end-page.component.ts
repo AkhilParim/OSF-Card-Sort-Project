70cec9c4b617e35a26cd99f3aa22366b5dee99d8
AkhilParim <akhil.parim@gmail.com>
2023-06-21 16:26:05 +0530

    prompt on end page

--- Frontend/src/app/components/end-page/end-page.component.ts
+++ Frontend/src/app/components/end-page/end-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AppService } from 'src/app/app.service';
 import { HttpService } from 'src/app/http.service';
 
@@ -9,18 +10,30 @@ import { HttpService } from 'src/app/http.service';
 })
 export class EndPageComponent implements OnInit {
   showPageLoader: Boolean = true;
+  promptDuration: number = 10;
 
-  constructor(private httpService: HttpService, private appService: AppService) { }
+  constructor(private httpService: HttpService, private appService: AppService, private router: Router) { }
 
   ngOnInit(): void {
     if(this.appService.placedCards.length > 0) { 
       this.httpService.saveParticipation().subscribe(res => {
         this.showPageLoader = false;
+        this.executePrompt();
       });
     } else {
       this.showPageLoader = false;
+      this.executePrompt();
     }
   }
 
-
+  executePrompt() {
+    let interval = setInterval(() => {
+      this.promptDuration -= 1;
+      if(this.promptDuration == -1) {
+        clearInterval(interval);
+        this.router.navigate(['/']);
+      }
+    }, 1000);
+  }
+  
 }
